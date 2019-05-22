b1d1b7d68eb228e6e4f6b5e5095775cb5ab20514
kian ceelie <kcceelie01@gmail.com>
2019-05-22 13:47:43 +0200

    ik ga pushen

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
     echo"Dit is mijn eerste ervaring met Git<hr>";
     echo"Dit is een nieuwe regel code<hr>";
     echo"testtest"
-    echo"dit is een test commit voor tijdrijzen"
+    echo"dit is een test commit voor tijdrijzen";
     
-    echo"tweede tijdrijs"
+    echo"tweede tijdrijs";
+    
+    echo"ik ga een nieuwe commit pushen naar GitHub";
 ?>
